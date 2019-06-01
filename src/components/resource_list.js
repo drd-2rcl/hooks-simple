f8547acad46f7bd6897df7f2e1c27b73dac458f7
drd-2rcl <drd.rclrd@gmail.com>
2019-06-01 13:49:09 -0300

    fetching a given resources

--- src/components/resource_list.js
+++ src/components/resource_list.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ResouceList extends Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    this.setState({ resources: response.data });
+    console.log(response);
+  }
+
   render() {
-    return <div>{this.props.resource}</div>
+    return <div>{this.state.resources.length}</div>
   }
 }
 
