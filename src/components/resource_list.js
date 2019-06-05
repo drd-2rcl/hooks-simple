1cb6aa80b89a57149d007875603d562507eba5a6
drd-2rcl <drd.rclrd@gmail.com>
2019-06-04 22:44:10 -0300

    code reuse

--- src/components/resource_list.js
+++ src/components/resource_list.js
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResouceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
-
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-    setResources(response.data);
-  }
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource])
-
-  return <div>{resources.length}</div>
+  
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${resource}`
+        );
+      
+        setResources(response.data);
+      })(resource);
+    },
+    [resource]
+  );
+  return resources
 }
 
-export default ResouceList;
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
+
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default ResourceList;
